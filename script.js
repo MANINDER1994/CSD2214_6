28d952ca683a46bcf66b586aace667ebc964e168
MANINDER1994 <43041112+MANINDER1994@users.noreply.github.com>
2019-01-14 16:56:56 -0500

    Class Activity-14 January,2019

---
+++ script.js
@@ -0,0 +1,12 @@
+// Name: Maninder Kaur
+//Date: January 14,2019
+//Student ID:C0744403
+//Course:CSD 2214 Section 6
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const MinutesSpan = clock.querySelector('.Minutes');
+const secondsSpan = clock.querySelector('.seconds');
